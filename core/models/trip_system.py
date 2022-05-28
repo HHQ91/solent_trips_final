755413b559cc1e679acff2c9118a9d64555b036f
HHQ91 <92577965+HHQ91@users.noreply.github.com>
2022-05-28 13:03:42 +0100

    login method is added to  the constructor

--- core/models/trip_system.py
+++ core/models/trip_system.py
@@ -6,12 +6,15 @@ from core.types.role_types import RoleTypes
 class TripSystem:
     __trips = []
     __users = []
+    __logged_in_user = None
 
-    def __init__(self):
+    def __init__(self, username, password):
         # add system users
         self.__users.append(User("TripCoordinator", "12345", RoleTypes.coordinator))
         self.__users.append(User("TripManager", "12345", RoleTypes.manager))
         self.__users.append(User("TripAdmin", "12345", RoleTypes.administrator))
+        # log the user in
+        self.__logging_in(username, password)
 
     def get_trips(self):
         return self.__trips
@@ -49,4 +52,11 @@ class TripSystem:
             if item.get_id() == id:
                 self.__trips.remove(item)
 
-
+    def __logging_in(self, username, password):
+        for i, user in self.__users:
+            if user.get_name() == username and user.get_password() == password:
+                self.__logged_in_user = user
+                break
+        # if logged-in user still null then throw an exception
+        if self.__logged_in_user is None:
+            raise Exception("Please use a valid account")
