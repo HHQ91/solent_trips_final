af10e4a869a5eca1d1c7b74b71ca6977dc14892a
HHQ91 <92577965+HHQ91@users.noreply.github.com>
2022-05-30 12:12:01 +0100

    adding test cases to trip and traveller classes

---
+++ tests/test_traveller.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from unittest import TestCase
+
+from core.models.payment import Payment
+from core.models.traveller import Traveller
+from core.models.trip import Trip
+from core.types.duration_types import DurationTypes
+from core.types.government_id_types import GovernmentIdTypes
+
+
+class TestTraveller(TestCase):
+    def test_add_payment(self):
+        # arrange
+        traveller = Traveller("traveller 1", "address", "1996/1/1", "+9647734018021")
+        trip = Trip("trip 1", datetime.now, DurationTypes.weekend)
+        payment = Payment(trip, 10.5)
+        # act
+        traveller.add_payment(payment)
+        # assert
+        self.assertEqual(len(traveller.payments), 1, "should have 1")
+        self.assertEqual(traveller.payments[0], payment, "should be the same as declared above")
+
+    def test_remove_payment(self):
+        # arrange
+        traveller = Traveller("traveller 1", "address", "1996/1/1", "+9647734018021")
+        trip = Trip("trip 1", datetime.now, DurationTypes.weekend)
+        payment1 = Payment(trip, 10.5)
+        payment2 = Payment(trip, 12.5)
+        traveller.assign_payments([payment1, payment2])
+        # act
+        traveller.remove_payment(payment1.id)
+        # assert
+        self.assertEqual(len(traveller.payments), 1, "should have 1")
+
+    def test_assign_payments(self):
+        # arrange
+        traveller = Traveller("traveller 1", "address", "1996/1/1", "+9647734018021")
+        trip = Trip("trip 1", datetime.now, DurationTypes.weekend)
+        payment1 = Payment(trip, 10.5)
+        payment2 = Payment(trip, 12.5)
+        # act
+        traveller.assign_payments([payment1, payment2])
+        # assert
+        self.assertEqual(len(traveller.payments), 2, "should have 2")
+
+    def test_add_government_id(self):
+        # arrange
+        traveller = Traveller("traveller 1", "address", "1996/1/1", "+9647734018021")
+        gov_id = "Gov_Id"
+        gov_id_type = GovernmentIdTypes.passport
+        # act
+        traveller.add_government_id(gov_id, gov_id_type)
+        # assert
+        self.assertEqual(traveller.government_id, gov_id)
+        self.assertEqual(traveller.government_id_Type, gov_id_type)
---
+++ tests/test_trip.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+from unittest import TestCase
+
+from core.models.traveller import Traveller
+from core.models.trip import Trip
+from core.models.trip_leg import TripLeg
+from core.models.user import User
+from core.types.duration_types import DurationTypes
+from core.types.role_types import RoleTypes
+from core.types.transport_mode_types import TransportModeTypes
+
+
+class TestTrip(TestCase):
+    def test_assign_coordinator(self):
+        # arrange
+        trip = Trip("trip 1", datetime.now, DurationTypes.weekend)
+        coordinator = User("username", "pass", RoleTypes.coordinator)
+        # act
+        trip.assign_coordinator(coordinator)
+        # assert
+        self.assertEqual(trip.coordinator, coordinator, "should have coordinator same as declared above")
+
+    def test_add_traveller(self):
+        # arrange
+        trip = Trip("trip 1", datetime.now, DurationTypes.weekend)
+        traveller = Traveller("traveller", "address", "1996/1/1", "+9647734018021")
+        # act
+        trip.add_traveller(traveller)
+        # assert
+        self.assertEqual(len(trip.travellers), 1, "should have 1")
+        self.assertEqual(trip.travellers[0], traveller, "should be the same as declared above")
+
+    def test_remove_traveller(self):
+        # arrange
+        trip = Trip("trip 1", datetime.now, DurationTypes.weekend)
+        traveller1 = Traveller("traveller 1", "address", "1996/1/1", "+9647734018021")
+        traveller2 = Traveller("traveller 2", "address", "1996/1/1", "+9647734018021")
+        trip.assign_travellers([traveller1, traveller2])
+        # act
+        trip.remove_traveller(traveller1.id)
+        # assert
+        self.assertEqual(len(trip.travellers), 1, "should have 1")
+
+    def test_assign_travellers(self):
+        # arrange
+        trip = Trip("trip 1", datetime.now, DurationTypes.weekend)
+        traveller1 = Traveller("traveller 1", "address", "1996/1/1", "+9647734018021")
+        traveller2 = Traveller("traveller 2", "address", "1996/1/1", "+9647734018021")
+        # act
+        trip.assign_travellers([traveller1, traveller2])
+        # assert
+        self.assertEqual(len(trip.travellers), 2, "should have 2")
+
+    def test_add_trip_leg(self):
+        # arrange
+        trip = Trip("trip 1", datetime.now, DurationTypes.weekend)
+        trip_leg = TripLeg("start location", "destination", TransportModeTypes.taxi)
+        # act
+        trip.add_trip_leg(trip_leg)
+        # assert
+        self.assertEqual(len(trip.trip_legs), 1, "should have 1")
+        self.assertEqual(trip.trip_legs[0], trip_leg, "should be the same as declared above")
+
+    def test_remove_trip_leg(self):
+        # arrange
+        trip = Trip("trip 1", datetime.now, DurationTypes.weekend)
+        trip_leg1 = TripLeg("start location 1", "destination 1", TransportModeTypes.taxi)
+        trip_leg2 = TripLeg("start location 2", "destination 2", TransportModeTypes.taxi)
+        trip.assign_trip_legs([trip_leg1, trip_leg2])
+        # act
+        trip.remove_trip_leg(trip_leg1.id)
+        # assert
+        self.assertEqual(len(trip.trip_legs), 1, "should have 1")
+
+    def test_assign_trip_legs(self):
+        # arrange
+        trip = Trip("trip 1", datetime.now, DurationTypes.weekend)
+        trip_leg1 = TripLeg("start location 1", "destination 1", TransportModeTypes.taxi)
+        trip_leg2 = TripLeg("start location 2", "destination 2", TransportModeTypes.taxi)
+        # act
+        trip.assign_trip_legs([trip_leg1, trip_leg2])
+        # assert
+        self.assertEqual(len(trip.trip_legs), 2, "should have 2")
