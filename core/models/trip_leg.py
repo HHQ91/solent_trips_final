3a29ce09ead8e605658f34a49ff31fb464be8170
HHQ91 <92577965+HHQ91@users.noreply.github.com>
2022-05-31 23:23:31 +0100

    add point_of_interest to trip leg class

--- core/models/trip_leg.py
+++ core/models/trip_leg.py
@@ -6,10 +6,10 @@ class TripLeg:
     start_location = None
     destination = None
     mode_of_transport = None  # plane, ferry, coach, or taxi
+    point_of_interest = []  # we could make it class with name and geo location
 
     def __init__(self, start_location, destination, mode_of_transport):
         self.id = uuid.uuid4()
         self.start_location = start_location
-        self.destination = destination,
+        self.destination = destination
         self.mode_of_transport = mode_of_transport
-
